fca9e8d2e073c7e4630c84523fbdb60bd3f48dc3
Acatrinei Ștefan <stefan.aca74@gmail.com>
2024-12-04 21:40:55 +0200

    added groundwork for sensor data retrieval
    Files included but not completed. To be left as is

--- IncidentResponseAPI/IncidentResponseAPI/Scheduling/DataRetrievalJob.cs
+++ IncidentResponseAPI/IncidentResponseAPI/Scheduling/DataRetrievalJob.cs
@@ -1,6 +1,43 @@
-﻿namespace IncidentResponseAPI.Scheduling;
+﻿using Quartz;
+using System;
+using System.Formats.Asn1;
+using System.Threading.Tasks;
+using IncidentResponseAPI.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+//TODO - Completion of feature after email service link with backend application
+namespace IncidentResponseAPI.Scheduling;
 
-public class DataRetrievalJob
+public class DataRetrievalJob : IJob
 {
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<DataRetrievalJob> _logger;
     
+    public DataRetrievalJob(IServiceProvider serviceProvider, ILogger<DataRetrievalJob> logger)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var sensorId = context.JobDetail.JobDataMap.GetInt("SensorId");
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<IncidentResponseContext>();
+            var sensor = await dbContext.Sensors.FindAsync(sensorId);
+
+            if (sensor != null && sensor.isEnabled)
+            {
+                //Perform data retrieval logic here
+                _logger.LogInformation($"Data retrieval for sensor {sensor.SensorName} is complete.");
+                
+                //Placeholder for actual data retrieval logic
+                //example: var data = await RetrieveDataFromSensor(sensor);
+                
+                sensor.LastRunAt = DateTime.Now;
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
 }
--- IncidentResponseAPI/IncidentResponseAPI/Services/RetrievalLogic.cs
+++ IncidentResponseAPI/IncidentResponseAPI/Services/RetrievalLogic.cs
@@ -1,6 +1,12 @@
-﻿namespace IncidentResponseAPI.Services;
+﻿using IncidentResponseAPI.Models;
+
+namespace IncidentResponseAPI.Services;
 
 public class RetrievalLogic
 {
-    
+    public async Task<string> RetrieveDataFromSensor(SensorsModel sensor)
+    {
+        await Task.Delay(1000);
+        return "data";
+    }
 }
