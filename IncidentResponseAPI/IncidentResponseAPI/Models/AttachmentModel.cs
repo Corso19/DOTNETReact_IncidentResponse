fe624a191eacb1286f82d2e6abd5ae944629740c
Acatrinei Ștefan <58533122+Corso19@users.noreply.github.com>
2024-12-12 20:10:41 +0200

    Refactored EventsModel and AttachmentModel
    also added migration for the model changes in the database

---
+++ IncidentResponseAPI/IncidentResponseAPI/Models/AttachmentModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IncidentResponseAPI.Models;
+
+public class AttachmentModel
+{
+    [Key]
+    public int AttachmentId { get; set; }
+    public string Name { get; set; }
+    public int Size { get; set; }
+    public byte[] Content { get; set; }
+    public int EventId { get; set; }
+    //public EventsModel Event { get; set; }    
+    
+}
--- IncidentResponseAPI/IncidentResponseAPI/Models/EventsModel.cs
+++ IncidentResponseAPI/IncidentResponseAPI/Models/EventsModel.cs
@@ -5,12 +5,15 @@ namespace IncidentResponseAPI.Models
     public class EventsModel
     {
         [Key]
-        public int EventId { get; set; } // This will be recognized as the primary key
+        public int EventId { get; set; }
         public int SensorId { get; set; }
         public SensorsModel Sensor { get; set; }
-        public string TypeName { get; set; }
+        public string TypeName { get; set; } = "Email";
+        public string Subject { get; set; }
+        public string Sender { get; set; }
         public string Details { get; set; }
         public DateTime Timestamp { get; set; } = DateTime.Now;
         public bool isProcessed { get; set; } = false;
+        public string MessageId { get; set; }   
     }
 }
